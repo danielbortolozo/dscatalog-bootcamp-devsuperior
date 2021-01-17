cda00decea225fbcfc6401ba175bec1abc08f060
Daniel Rocca Bortolozo <danielbortolozo@hotmail.com>
2021-01-17 14:22:54 -0300

    Adding CategoryResource

---
+++ backend/src/main/java/br/com/sisdb/dscatalog/resources/CategoryResource.java
@@ -0,0 +1,46 @@
+package br.com.sisdb.dscatalog.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.sisdb.dscatalog.entities.Category;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+   
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll()  {
+		List<Category> list = new ArrayList<>();
+		
+		list.add(new Category(1L, "Books"));
+		list.add(new Category(1L, "Electronics"));		
+		
+		return ResponseEntity.ok(list); 
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
