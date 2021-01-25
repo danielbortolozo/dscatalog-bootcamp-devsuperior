d5b0bb6e034405b86ce3483ba22e5a71bd6f6dfe
Daniel Rocca Bortolozo <danielbortolozo@hotmail.com>
2021-01-25 10:30:30 -0300

    Adding componentLoader

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './styles.scss';
 import ProductCard from './components/ProductCard';
+import ProductCardLoader from './components/PoductCatalogLoader';
 import { makeRequest } from '../../core/utils/request';
 import { ProductsResponse } from '../../core/types/Product';
 import { Link } from 'react-router-dom';
@@ -11,15 +12,19 @@ const Catalog = () => {
  // quando o componente iniciar, buscar a lista de produtos.
  // 
  const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+ const [isLoading, setIsLoading] = useState(false);
  useEffect(() => {
    const params = {
      page: 0,
      linesPerPage: 12
    }
-   console.log('buscando products ...');
+   setIsLoading(true);
    makeRequest({url: '/products', params})
      
      .then(response => setProductsResponse(response.data))
+     .finally(() => {
+        setIsLoading(false);
+     })
  }, []);
    return (
     <div className="catalog-container">
@@ -27,11 +32,14 @@ const Catalog = () => {
         Catálago de produtos
       </h1>
       <div className="catalog-products">
-        {productsResponse?.content.map(product => (
-          <Link to={`/products/${product.id}`} key={product.id}>
-            <ProductCard product={product}/>
-          </Link>  
-        ))}                
+        {isLoading ? <ProductCardLoader /> : (
+          productsResponse?.content.map(product => (
+            <Link to={`/products/${product.id}`} key={product.id}>
+              <ProductCard product={product}/>
+            </Link>  
+          ))
+        )}
+                       
       </div>  
     </div> 
   );
