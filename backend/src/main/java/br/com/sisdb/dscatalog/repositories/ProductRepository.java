a364dd5dc72d36a5429f6ed213d987dfe5988291
Daniel Rocca Bortolozo <danielbortolozo@hotmail.com>
2021-02-20 12:10:55 -0300

    Fix: Postgres compatibility.

--- backend/src/main/java/br/com/sisdb/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/br/com/sisdb/dscatalog/repositories/ProductRepository.java
@@ -1,11 +1,30 @@
 package br.com.sisdb.dscatalog.repositories;
 
+import br.com.sisdb.dscatalog.entities.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.sisdb.dscatalog.entities.Product;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
+    @Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats" +
+            " WHERE (COALESCE(:categories)IS NULL OR cats IN :categories) AND " +
+            "(:name = '' OR LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%')) )")
+    Page<Product> find(List<Category> categories, String name, Pageable pageable);
 }
+
+
+
+
+
+
+
+
+
--- backend/src/main/java/br/com/sisdb/dscatalog/services/ProductService.java
+++ backend/src/main/java/br/com/sisdb/dscatalog/services/ProductService.java
@@ -1,5 +1,7 @@
 package br.com.sisdb.dscatalog.services;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
@@ -30,8 +32,9 @@ public class ProductService {
 	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly = true)
-	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
-		Page<Product> list = repository.findAll(pageRequest);
+	public Page<ProductDTO> findAllPaged(Long categoryId, String name,  PageRequest pageRequest) {
+		List<Category> categories = (categoryId == 0) ? null : Arrays.asList(categoryRepository.getOne(categoryId));
+		Page<Product> list = repository.find(categories, name,  pageRequest);
 		return list.map(x -> new ProductDTO(x));
 	}
 
