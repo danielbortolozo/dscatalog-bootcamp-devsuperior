d3a3ae58c8b21fc472adec9c56ded7ceeec11387
Daniel Rocca Bortolozo <danielbortolozo@hotmail.com>
2021-04-17 02:12:42 -0300

    Integrate product listing backend

---
+++ front-web/src/pages/Admin/components/Products/Card/index.tsx
@@ -0,0 +1,60 @@
+
+import ProductPrice from 'core/components/productprice';
+import { Product } from 'core/types/Product';
+import React from 'react';
+import './styles.scss';
+
+type Props = {
+   product: Product;
+}
+
+
+const Card = ({ product }: Props) => {
+ 
+     return (
+       <div className="card-base product-card-admin ">
+           <div className="row">
+              <div className="col-2 text-center border-right py-3">
+                 <img 
+                    src={product.imgUrl}
+                    alt={product.name}  
+                    className="product-card-image-admin"
+                 />
+                 
+
+              </div>
+              <div className="col-7 py-3">
+                 <h3 className="product-card-name-admin">
+                     {product.name}
+                 </h3>
+                 <ProductPrice price={product.price} />
+                 <div>
+                    {product.categories.map(category => (
+                       <span className="badge badge-pill badge-secondary mr-2"> Categoria1</span>
+                    ))}
+                 </div>
+                 
+              </div>
+              <div className="col-3 pt-3 pr-5">
+                    <button
+                      type="button"
+                      className="btn btn-outline-secondary btn-block border-radius-10 mb-3 btn-edit"
+                    > 
+                    EDITAR
+                     </button>
+                     <button
+                      type="button"
+                      className="btn btn-outline-danger btn-block border-radius-10 "
+                    > 
+                    EXCLUIR 
+                     </button>
+
+              </div>
+           </div>
+       </div>
+     );
+
+}
+
+
+export default Card;
--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import Pagination from 'core/components/Pagination';
+import { ProductsResponse } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import Card from '../Card';
 
 const List = () => {
-    const history = useHistory();
+
+  const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [activePage, setActivePage] = useState(0);
+  const history = useHistory();
+
+
+ console.log(productsResponse)
+  useEffect(() => {
+    const params = {
+      page: activePage,
+      linesPerPage: 4
+    }
+    setIsLoading(true);
+    makeRequest({url: '/products', params})
+      
+      .then(response => setProductsResponse(response.data))
+      .finally(() => {
+         setIsLoading(false);
+      })
+  }, [activePage]);
+
+  
 
     const handleCreate = () => {
       console.log('passei aqui...')
@@ -11,10 +37,27 @@ const List = () => {
 
     return (
         <div className="admin-products-list">
+          
            <button className="btn btn-primary btn-lg" onClick={handleCreate}>
              Adicionar
            </button>    
-        </div>
+        
+           <div className="admin-list-container">
+              { productsResponse?.content.map(product => (
+                    <Card product={product} key={product.id}/>
+                    ))
+              }
+           </div>
+           { productsResponse && (
+        <Pagination 
+          totalPages={productsResponse.totalPages} 
+          activePage={activePage} 
+          onChange={page => setActivePage(page)} 
+        />
+          
+        )}
+           
+        </div>  
     );
 };
 
